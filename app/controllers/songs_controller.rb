9ce88af4e8ace5ff0c9024cdb9c5bb09c7348937
Dee Race <deerace12@gmail.com>
2018-06-15 15:02:24 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,15 +1,18 @@
 class SongsController < ApplicationController
 
   def create
-
+    @song = Song.create(params.require(:song))
+    redirect_to song_path(@song)
   end
 
   def update
-
+    @song = Song.find(params[:id])
+    @song.update(params.require(:song))
+    redirect_to song_path(@song)
   end
 
   def show
-
+    @song = Song.find(params[:id])
   end
 
 end 
